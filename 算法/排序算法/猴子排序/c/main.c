4d1bef6d9d250b0522adf5b41cd86c2eeb7ecc40
FrancisX <1340058590@qq.com>
2019-09-05 18:13:10 +0800

    猴子排序

---
+++ 算法/排序算法/猴子排序/c/main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+//判断是否排序完毕
+int isSorted(int arr[],int size){
+    int i;
+    for(i = 1; i < size; i++)
+        if(arr[i] < arr[i-1]) return 0;
+    return 1;
+}
+
+//排序主体
+void monkey_sort(int arr[],int size){
+    int rnd1,rnd2;
+    while(!isSorted(arr,size)){
+        rnd1 = rand() % size;
+        rnd2 = rand() % size;
+        int tmp = arr[rnd1];
+        arr[rnd1] = arr[rnd2];
+        arr[rnd2] = tmp;
+    }
+}
+
+int main(){
+    srand(time(NULL));
+    int arr[] = {1,3,4,0,0,5,8,5,9,0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int i;
+
+    puts("排序前");for(i = 0; i < size; i++) printf("%d ",arr[i]);puts("");
+    monkey_sort(arr,size);
+    puts("排序后");for(i = 0; i < size; i++) printf("%d ",arr[i]);puts("");
+    return 0;
+}
