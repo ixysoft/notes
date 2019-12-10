d8faafca5d873f182d87ebefa6b7aa4c16c1a9e2
FrancisX <1340058590@qq.com>
2019-12-10 23:07:49 +0800

    不使用除号实现除法

--- DailyCodingProblems/380_divide_Nextdoor/python/main.py
+++ DailyCodingProblems/380_divide_Nextdoor/python/main.py
@@ -2,5 +2,21 @@
 #python3
 
 def divide(a,b):
-    if a == 0:
-        return 0
+    if b == 0:
+        return None
+    elif b == 1:
+        return a
+    left = 0
+    right = a
+    while left < right:
+        cur = (left + right)>>1
+        res = cur * b
+        if res <= a and a - res < b:
+            left = right = cur
+        elif res > a:
+            right = cur - 1
+        else:
+            left = cur + 1
+    return (left,a - left * b)
+
+print(divide(100,31))
